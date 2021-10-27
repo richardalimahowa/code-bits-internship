6486eef98cc6e01f1ea62d527c18227885470eab
Richard <richardsserubombwe@gmail.com>
2021-10-27 04:47:56 -0700

    day 1 hello world .py

---
+++ day 1 hello world.py
@@ -0,0 +1,4 @@
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+print(input_string)
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
